8e6e93c5d6e1bba8db3a5fff7fc6e2a1897d6a2a
Subve <60174799+Subve@users.noreply.github.com>
2022-12-26 13:19:58 +0100

    Add project files.

---
+++ CvManagementMVC.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CvManagementMVC.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
