89c1e67c5da6921bb0fa0dffb07b1ee1b51f7ad7
Subve <60174799+Subve@users.noreply.github.com>
2023-01-04 01:22:03 +0100

    added mapper  for candidateForListVm

---
+++ CvManagementMVC.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CvManagementMVC.Application.Mapping
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            ApplyMappingsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            var types=assembly.GetExportedTypes()
+                .Where(t=>t.GetInterfaces().Any(i=>
+                i.IsGenericType&& i.GetGenericTypeDefinition() == typeof(IMapFrom<>)))
+                .ToList();
+        }
+    }
+}
--- CvManagementMVC.Application/ViewModels/Candidate/CandidateForListVm.cs
+++ CvManagementMVC.Application/ViewModels/Candidate/CandidateForListVm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AutoMapper;
+using CvManagementMVC.Application.Mapping;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +8,21 @@ using System.Threading.Tasks;
 
 namespace CvManagementMVC.Application.ViewModels.Candidate
 {
-    public class CandidateForListVm
+    public class CandidateForListVm : IMapFrom<CvManagementMVC.Domain.Model.Candidate>
     {
-        public int Id  { get; set; }
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public bool isActive { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<CvManagementMVC.Domain.Model.Candidate, CandidateForListVm>()
+                .ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id))
+                .ForMember(d => d.FirstName, opt => opt.MapFrom(s => s.FirstName))
+                .ForMember(d => d.LastName, opt => opt.MapFrom(s => s.LastName))
+                .ForMember(d => d.isActive, opt => opt.MapFrom(s => s.isActive));
+        }
 
     }
 }
